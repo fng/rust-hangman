9163bbaa874d35321276d5283f94ae18b7df79af
fng <daniel.wydler@gmail.com>
2022-09-15 11:08:17 +0200

    introduce struct HangmanGame

--- src/main.rs
+++ src/main.rs
@@ -62,69 +62,87 @@ impl Word {
     }
 }
 
+
+struct HangmanGame {
+    word_to_find: Word,
+    remaining_attempts: u8,
+}
+
+impl HangmanGame {
+    fn new_random_from_file(file_name: &str, remaining_attempts: u8) -> HangmanGame {
+        return HangmanGame {
+            word_to_find: Word::new_random_from_file(&file_name),
+            remaining_attempts,
+        };
+    }
+
+    fn start(&mut self) {
+        loop {
+            println!("\nAttempts left: {}", self.remaining_attempts);
+            Self::print_word_to_find(&self.word_to_find);
+
+            println!("Enter a letter: ");
+            let input = Self::read_first_char_from_console();
+
+            let mut input_matched = false;
+
+            for letter in self.word_to_find.letters.iter_mut() {
+                if letter.character == input {
+                    letter.is_revealed = true;
+                    input_matched = true;
+                }
+            }
+
+            if !input_matched {
+                self.remaining_attempts -= 1;
+            }
+
+            match self.word_to_find.game_state(self.remaining_attempts) {
+                GameState::Won => {
+                    println!("Congratulation you won!");
+                    break;
+                }
+                GameState::Lost => {
+                    println!("Bad lock, you are out of attempts!");
+                    println!("The word was: {}", self.word_to_find.word);
+                    println!("Try again!");
+                    break;
+                }
+                GameState::InProgress => {
+                    continue;
+                }
+            }
+        }
+    }
+
+    fn read_first_char_from_console() -> char {
+        let mut input: String = String::new();
+        stdin().read_line(&mut input).expect("Invalid input");
+        return input.chars().next().expect("Can't get first char from input");
+    }
+
+    fn print_word_to_find(word: &Word) {
+        let mut output = String::new();
+        output.push_str("Word to find: ");
+        for letter in &word.letters {
+            output.push(' ');
+            if letter.is_revealed {
+                output.push(letter.character);
+            } else {
+                output.push('_');
+            }
+        }
+        println!("{}", output);
+    }
+}
+
 const TOTAL_ATTEMPTS: u8 = 5;
 
 fn main() {
-    let mut word_to_find = Word::new_random_from_file("words.txt");
-
-    let mut remaining_attempts: u8 = TOTAL_ATTEMPTS;
-
-    loop {
-        println!("\nAttempts left: {}", remaining_attempts);
-        print_word_to_find(&word_to_find);
-
-        println!("Enter a letter: ");
-        let input = read_first_char_from_console();
-
-        let mut input_matched = false;
-
-        for letter in word_to_find.letters.iter_mut() {
-            if letter.character == input {
-                letter.is_revealed = true;
-                input_matched = true;
-            }
-        }
-
-        if !input_matched {
-            remaining_attempts -= 1;
-        }
-
-        match word_to_find.game_state(remaining_attempts) {
-            GameState::Won => {
-                println!("Congratulation you won!");
-                break;
-            }
-            GameState::Lost => {
-                println!("Bad lock, you are out of attempts!");
-                println!("The word was: {}", word_to_find.word);
-                println!("Try again!");
-                break;
-            }
-            GameState::InProgress => {
-                continue;
-            }
-        }
-    }
+    let mut game = HangmanGame::new_random_from_file("words.txt", TOTAL_ATTEMPTS);
+    game.start();
 }
 
 
-fn read_first_char_from_console() -> char {
-    let mut input: String = String::new();
-    stdin().read_line(&mut input).expect("Invalid input");
-    return input.chars().next().expect("Can't get first char from input");
-}
 
-fn print_word_to_find(word: &Word) {
-    let mut output = String::new();
-    output.push_str("Word to find: ");
-    for letter in &word.letters {
-        output.push(' ');
-        if letter.is_revealed {
-            output.push(letter.character);
-        } else {
-            output.push('_');
-        }
-    }
-    println!("{}", output);
-}
 
