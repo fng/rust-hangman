cea69fb3ae2fcfaa69d882b7974d0f9bac83de18
fng <daniel.wydler@gmail.com>
2022-09-15 10:56:28 +0200

    introduce struct Word

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 extern crate rand;
+extern crate core;
 
 use std::fs::File;
 use std::io::prelude::*;
@@ -17,27 +18,67 @@ enum GameState {
     Lost,
 }
 
+struct Word {
+    word: String,
+    letters: Vec<Letter>,
+}
+
+impl Word {
+    fn game_state(&self, remaining_attempts: u8) -> GameState {
+        let mut all_revealed = true;
+        for letter in &self.letters {
+            if !letter.is_revealed {
+                all_revealed = false;
+            }
+        }
+
+        return if all_revealed {
+            GameState::Won
+        } else if remaining_attempts > 0 {
+            GameState::InProgress
+        } else {
+            GameState::Lost
+        };
+    }
+
+    fn new_random_from_file(file_name: &str) -> Word {
+        let mut file = File::open(file_name).expect(&format!("Can't open {}", file_name));
+        let mut content = String::new();
+        file.read_to_string(&mut content).expect(&format!("Can't read from {}", file_name));
+        let words: Vec<String> = content.split(',')
+            .map(|word| word.to_string())
+            .collect();
+        let random_index = rand::thread_rng().gen_range(0..words.len());
+        let word = words.get(random_index).expect(&format!("Could not select word at index {}", random_index)).to_string();
+
+        return Word {
+            word: String::from(&word),
+            letters: word.chars()
+                .map(|char| Letter {
+                    character: char,
+                    is_revealed: false,
+                }).collect(),
+        };
+    }
+}
+
 const TOTAL_ATTEMPTS: u8 = 5;
 
 fn main() {
-    let words = read_words_from_file("words.txt");
-
-    let selected_word = select_random_word(&words);
-
-    let mut letters = word_to_letters(&selected_word);
+    let mut word_to_find = Word::new_random_from_file("words.txt");
 
     let mut remaining_attempts: u8 = TOTAL_ATTEMPTS;
 
     loop {
         println!("\nAttempts left: {}", remaining_attempts);
-        print_letters(&letters);
+        print_word_to_find(&word_to_find);
 
         println!("Enter a letter: ");
         let input = read_first_char_from_console();
 
         let mut input_matched = false;
 
-        for letter in letters.iter_mut() {
+        for letter in word_to_find.letters.iter_mut() {
             if letter.character == input {
                 letter.is_revealed = true;
                 input_matched = true;
@@ -48,14 +89,14 @@ fn main() {
             remaining_attempts -= 1;
         }
 
-        match calculate_game_state(remaining_attempts, &letters) {
+        match word_to_find.game_state(remaining_attempts) {
             GameState::Won => {
                 println!("Congratulation you won!");
                 break;
             }
             GameState::Lost => {
                 println!("Bad lock, you are out of attempts!");
-                println!("The word was: {}", selected_word);
+                println!("The word was: {}", word_to_find.word);
                 println!("Try again!");
                 break;
             }
@@ -66,20 +107,6 @@ fn main() {
     }
 }
 
-fn read_words_from_file(filename: &str) -> Vec<String> {
-    let mut file = File::open(filename).expect(&format!("Can't open {}", filename));
-    let mut content = String::new();
-    file.read_to_string(&mut content).expect(&format!("Can't read from {}", filename));
-
-    return content.split(',')
-        .map(|word| word.to_string())
-        .collect();
-}
-
-fn select_random_word(words: &Vec<String>) -> String {
-    let random_index = rand::thread_rng().gen_range(0..words.len());
-    return words.get(random_index).expect(&format!("Could not select word at index {}", random_index)).to_string();
-}
 
 fn read_first_char_from_console() -> char {
     let mut input: String = String::new();
@@ -87,18 +114,10 @@ fn read_first_char_from_console() -> char {
     return input.chars().next().expect("Can't get first char from input");
 }
 
-fn word_to_letters(word: &str) -> Vec<Letter> {
-    return word.chars()
-        .map(|char| Letter {
-            character: char,
-            is_revealed: false,
-        }).collect();
-}
-
-fn print_letters(letters: &Vec<Letter>) {
+fn print_word_to_find(word: &Word) {
     let mut output = String::new();
     output.push_str("Word to find: ");
-    for letter in letters {
+    for letter in &word.letters {
         output.push(' ');
         if letter.is_revealed {
             output.push(letter.character);
@@ -109,30 +128,3 @@ fn print_letters(letters: &Vec<Letter>) {
     println!("{}", output);
 }
 
-fn calculate_game_state(remaining_attempts: u8, letters: &Vec<Letter>) -> GameState {
-    let mut all_revealed = true;
-    for letter in letters {
-        if !letter.is_revealed {
-            all_revealed = false;
-        }
-    }
-
-    return if all_revealed {
-        GameState::Won
-    } else if remaining_attempts > 0 {
-        GameState::InProgress
-    } else {
-        GameState::Lost
-    };
-}
-
-#[test]
-fn word_to_letters_test(){
-    let string = String::from("daniel");
-    let letters = word_to_letters(&string);
-
-    //all letters are not revealed
-    for letter in letters {
-        assert!(!letter.is_revealed)
-    }
-}
